47f5856c734bae830454fbc5abe3abf233601606
BfortF5 <brettfort@hotmail.com>
2023-10-14 17:39:51 -0700

    adding files

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+document.write('Hello!, welcome to my Baseball Card Website!!!');
+
+let userFrom = prompt("Where are you from?");
+
+if(userFrom === 'Seattle'){
+    document.write('Wow, your favorite team must be a Seattle Marnier or Seahawlk fan.');
+} else {
+    document.write("Welcome from " + userFrom + "!!!");
+}
