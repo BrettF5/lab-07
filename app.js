aa61033a6cdf76c7f1e0e51639bfd1edd09afb77
BfortF5 <brettfort@hotmail.com>
2023-10-14 18:42:19 -0700

    updating lab07

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 'use strict';
 
-document.write('Hello!, welcome to my Baseball Card Website!!!');
+// document.write('Hello!, welcome to my Baseball Card Website!!!');
 
-let userFrom = prompt("Where are you from?");
+// let userFrom = prompt("Where are you from?");
 
-if(userFrom === 'Seattle'){
-    document.write('Wow, your favorite team must be a Seattle Marnier or Seahawlk fan.');
-} else {
-    document.write("Welcome from " + userFrom + "!!!");
-}
+// if(userFrom === 'Seattle'){
+//     document.write('Wow, your favorite team must be a Seattle Marnier or Seahawlk fan.');
+// } else {
+//     document.write("Welcome from " + userFrom + "!!!");
+// }
+
+function getLocation(){
+    let = userFrom('Where are you from?');
+    return userFrom
+ }
+
+
+ function locationMessage(userFrom){
+    if(userFrom ''){
+        userFrom = prompt('Please tell me where your from if you want to');
+    }
+
+    if(userFrom === 'Seattle'){
+        document.write("Welcome Seattle Mariner Fan!!!");
+    } else if (userFrom === 'New York'){
+        document.write('Welcome New York Yankee Fan!!!');
+    } else if (userFrom === 'Boston'){
+        document.write('Welcome Boston Red Sox Fan!!! Sorry for a bad season...') ;    
+    } else {
+        document.write('Sorry, do not have a message for your location. Please enter Seattle, New York or Boston for a different resoonse.');
+    }
+    
+ }
+
