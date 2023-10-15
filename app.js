7146d756ad182e84ee6d529dc73a67c165bf2497
BfortF5 <brettfort@hotmail.com>
2023-10-14 20:03:24 -0700

    updating

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 'use strict';
 
-// document.write('Hello!, welcome to my Baseball Card Website!!!');
-
-// let userFrom = prompt("Where are you from?");
-
-// if(userFrom === 'Seattle'){
-//     document.write('Wow, your favorite team must be a Seattle Marnier or Seahawlk fan.');
-// } else {
-//     document.write("Welcome from " + userFrom + "!!!");
-// }
-
 function getLocation(){
     let userFrom = prompt('Where are you from?');
     return userFrom;
